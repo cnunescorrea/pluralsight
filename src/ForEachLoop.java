14eef5531741d3f4ae441101e596cd255b89e1d5
cnunescorrea <cnunescorrea@users.noreply.github.com>
2020-10-15 15:16:10 -0400

    task ReturnMethod

--- src/ForEachLoop.java
+++ src/ForEachLoop.java
@@ -1,7 +1,6 @@
-public class Array {
+public class ForEachLoop {
 
     public static void main(String[] args) {
-
         double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
         double[] rightVals = {50.0d, 92.0d, 17.0d, 3.0d};
         char[] opCodes = {'d', 'a', 's', 'm'};
@@ -19,8 +18,7 @@ public class Array {
                     results[i] = leftVals[i] * rightVals[i];
                     break;
                 case 'd':
-                    results[i] = leftVals[i] / rightVals[i];
-                   // results[i] = rightVals[i] != 0 ? leftVals[i] / rightVals[i] : 0.0d;
+                    results[i] = rightVals[i] != 0 ? leftVals[i] / rightVals[i] : 0.0d;
                     break;
                 default:
                     System.out.println("Invalid opCode:" + opCodes[i]);
@@ -28,6 +26,8 @@ public class Array {
                     break;
             }
         }
-        System.out.println(results);
+
+        for(double currentResult : results)
+            System.out.println(currentResult);
     }
 }
